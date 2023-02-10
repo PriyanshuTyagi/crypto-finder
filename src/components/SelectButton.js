364b26530d20d8306f65db5cec5b46ddbf729e73
Priyanshu Tyagi <priyanshutyagi5629@gmail.com>
2023-02-10 21:29:52 +0530

    crypto-finder

---
+++ src/components/SelectButton.js
@@ -0,0 +1,37 @@
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid #5DA3FA",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "#23C4ED" : "",
+      color: selected ? "black" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "#23C4ED",
+        color: "black",
+      },
+      width: "22%",
+      //   margin: 5,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
+
+
+
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBcjn-Q4XTj2u0KBL53L2eKlZoIriK2FlA",
+  authDomain: "crypto-tracker-313ef.firebaseapp.com",
+  projectId: "crypto-tracker-313ef",
+  storageBucket: "crypto-tracker-313ef.appspot.com",
+  messagingSenderId: "859481581700",
+  appId: "1:859481581700:web:26cd4117f18bf8520dbad1",
+};
+
+export default firebaseConfig;
